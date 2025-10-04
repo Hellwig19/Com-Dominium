157cbedc83969d7f473a514916d67b563fd6584c
Hellwig19 <octavio.hellwig1@gmail.com>
2025-10-04 16:21:45 -0300

    Dependencias e primeiras configs

---
+++ src/pages/Home_portaria.tsx
@@ -0,0 +1,7 @@
+export default function HomePortaria() {
+  return (
+    <div className="bg-blue-400 h-screen w-screen flex items-center justify-center text-5xl text-white">
+      <h1>Home Portaria</h1>
+    </div>
+  );
+}
---
+++ src/pages/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className="bg-gray-200 h-screen w-screen flex items-center justify-center text-5xl text-gray-800">
+      <h1>Login</h1>
+    </div>
+  );
+}
